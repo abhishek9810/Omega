c631837950fadc24b07538f026b5b87ad187afc8
abhishek9810 <68103947+abhishek9810@users.noreply.github.com>
2020-07-10 14:43:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyC7QZALnKzD8NceTpmsppvLjuCD_4VE3Ho",
+  authDomain: "abhishek-77d97.firebaseapp.com",
+  databaseURL: "https://abhishek-77d97.firebaseio.com",
+  projectId: "abhishek-77d97",
+  storageBucket: "abhishek-77d97.appspot.com",
+  messagingSenderId: "781003728437",
+  appId: "1:781003728437:web:b05fb6c717b375abc0f37c",
+  measurementId: "G-J9TK7VQ7MR"
 };
 
      
